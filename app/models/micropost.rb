f972901a32a3d20a34a6d78c1720984423f9c70e
yakkelol <yannick.tordoir@gmail.com>
2013-04-08 21:07:50 +0200

    merge

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -6,7 +6,6 @@ class Micropost < ActiveRecord::Base
 
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
-<<<<<<< HEAD
 
   def self.from_users_followed_by(user)
     followed_user_ids = "SELECT followed_id FROM relationships
@@ -14,6 +13,4 @@ class Micropost < ActiveRecord::Base
     where("user_id IN (#{followed_user_ids}) OR user_id = :user_id",
           user_id: user.id)
   end
-=======
->>>>>>> 46e06369fe494e2a14075571b85ce626d13e2ea4
 end
