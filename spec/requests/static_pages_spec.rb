f5dc4346acb4dc118b3c95d468c33394933f2bfc
yakkelol <yannick.tordoir@gmail.com>
2013-04-08 15:29:29 +0200

    added user feed

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,7 +7,7 @@ describe "Static pages" do
   describe "Home page" do
     before { visit root_path }
 
-    it { should have_selector('h1',    text: 'Sample App') }
+    it { should have_selector('h1', text: 'Sample App') }
     it { should have_selector('title', text: full_title('')) }
     it { should_not have_selector 'title', text: '| Home' }
   end
@@ -15,7 +15,7 @@ describe "Static pages" do
   describe "Help page" do
     before { visit help_path }
 
-    it { should have_selector('h1',    text: 'Help') }
+    it { should have_selector('h1', text: 'Help') }
     it { should have_selector('title', text: full_title('Help')) }
 
     describe "for signed-in users" do
@@ -32,20 +32,31 @@ describe "Static pages" do
           page.should have_selector("li##{item.id}", text: item.content)
         end
       end
+
+      describe "follower/following counts" do
+        let(:other_user) { FactoryGirl.create(:user) }
+        before do
+          other_user.follow!(user)
+          visit root_path
+        end
+
+        it { should have_link("0 following", href: following_user_path(user)) }
+        it { should have_link("1 followers", href: followers_user_path(user)) }
+      end
     end
   end
 
   describe "About page" do
     before { visit about_path }
 
-    it { should have_selector('h1',    text: 'About') }
+    it { should have_selector('h1', text: 'About') }
     it { should have_selector('title', text: full_title('About Us')) }
   end
 
   describe "Contact page" do
     before { visit contact_path }
 
-    it { should have_selector('h1',    text: 'Contact') }
+    it { should have_selector('h1', text: 'Contact') }
     it { should have_selector('title', text: full_title('Contact')) }
   end
 end
